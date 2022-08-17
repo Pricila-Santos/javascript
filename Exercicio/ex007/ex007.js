7229d2761af506ae6bbd05f0faf254a57616dc07
Pricila santos <pricilaebruno@gmail.com>
2022-08-17 10:32:36 -0300

    Exercício 010

---
+++ Exercicio/ex007/ex007.js
@@ -0,0 +1,7 @@
+var vel =80 //Condição simples
+console.log(`A velocidade so seu carro é de ${vel}km/hr`)
+if (vel > 60) {
+    console.log (`Você ultrapassou a velocidade premitida.MULTADO`)
+}
+console.log(`Dirija sempre com segurança`)
+
