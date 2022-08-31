df41ece2447ff5f6d83aceb36d1cdac0842519e5
Pricila santos <pricilaebruno@gmail.com>
2022-08-31 10:56:54 -0300

    desafio005

---
+++ Desafio/d005/acoes.js
@@ -0,0 +1,20 @@
+function converter() {
+    let bt =document.getElementById('converter')
+    let saida = document.getElementById('saida')
+    let m =Number(window.prompt('Digite a distancia em metros'))
+    let km =Number(m/1000)
+    let hm=Number(m/100)
+    let dam =Number(m/10)
+    let dm = Number(m*10)
+    let cm =Number(m*100)
+    let mm =Number(m*1000)
+
+    saida.innerHTML = (`<h1>A distancia de ${m} metros,corresponde a ...</h1>
+                           <p>${km} quilometros (km)</p>
+                           <p>${hm} hectometro (hm)</p>
+                           <p>${dam} Decâmetro (dam)</p>
+                           <p>${dm} Decímentro (dm)</p>
+                           <p>${cm} Centímetro (cm)</p>
+                           <p>${mm} Milímetro (mm)</p>         `)
+    
+}
